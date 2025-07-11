d85d2ce1bbbe151d64e96387c6e08e5d3af0f069
Arbenita <141353698+arbenitacanaj4@users.noreply.github.com>
2025-07-11 22:32:45 +0200

    pics and css organized

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ $isLoggedIn = isset($_SESSION['user']); // Assume you store user data in $_SESSI
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>HerMatchUp</title>
     <!-- <link rel="stylesheet" href="styles.css"> -->
-    <link rel="stylesheet" href="indexStyles.css">
+    <link rel="stylesheet" href="css/indexStyles.css">
     <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@400;500;600;700&family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
 </head>
